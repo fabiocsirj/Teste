952852d0be1305264f7911ab6ae27df7ef2023e2
fabioguimaraes <fabiocsirj@gmail.com>
2018-10-06 20:18:49 -0300

    Commit2

--- app/src/main/java/br/com/appwharehouse/teste/Home.java
+++ app/src/main/java/br/com/appwharehouse/teste/Home.java
@@ -5,7 +5,6 @@ import android.app.FragmentTransaction;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 public class Home extends AppCompatActivity implements Fragment1.OnSelectedItem {
